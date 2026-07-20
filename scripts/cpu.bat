e52a101948f4202406c7d541d4df121fabaf56b0
Jasper <jasper.sinclair@mail.com>
2026-07-20 14:04:26 +0200

    update scripts

---
+++ scripts/cpu.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d C:\DEV\NULLSTAR-TRAIN\TRAINING
+call nnue_cpu_env\Scripts\activate
---
+++ scripts/gpu.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d C:\DEV\NULLSTAR-TRAIN\TRAINING
+call nnue_gpu_env\Scripts\activate
